149b6cc10500288cbac37ea5a655a8870adf205d
idiotblin <arsenavh@gmail.com>
2025-03-20 14:08:40 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/arsenavkhadiev/SelfIntroduction.kt
@@ -0,0 +1,11 @@
+package hk.edu.hkbu.comp.comp4107.playground.arsenavkhadiev
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Arsen Avkhadiev.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am not interested in machine learning.")
+    }
+}
+
